e50c3f3f60540629af0861d0506210997ab66219
vbyk0v <79099116020@ya.ru>
2019-05-24 14:28:58 +0300

    Create RandomStringToInteger.py

---
+++ RandomStringToInteger.py
@@ -0,0 +1,16 @@
+import re
+
+str = 'temperature                         : 24 C'
+
+
+def rs(string_to_parse, one_number=True):
+    if one_number:
+        result = re.findall(r'\d+', string_to_parse)
+        numb = int(result[0])
+        print(type(numb))
+        return numb
+    elif not one_number:
+        pass
+
+
+print(rs(string_to_parse=str))
